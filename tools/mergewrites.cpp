6e09366ce215f347fb2ebc7d5811d3c5552c1e53
Jun He <junnhe@gmail.com>
2013-07-28 12:27:18 -0700

    now able to flush files to disks in mergewrites

--- tools/mergewrites.cpp
+++ tools/mergewrites.cpp
@@ -33,12 +33,14 @@ using namespace std;
 int main(int argc, char **argv)
 {
     // Handle the program arguments
-    if (argc < 5) {
+    if (argc < 7) {
         cout << "Usage: " << argv[0] 
              << " mapfilename" 
              << " FetchBuffer" 
              << " DoMerge"
              << " DoSort"
+             << " DoFlush" // save indices to disk
+             << " FlushDir" // where to save indices files
              << endl;
         exit(1);
     }
@@ -47,6 +49,8 @@ int main(int argc, char **argv)
     int iFetchSize = atoi(argv[2]);
     bool iDoMerge = (bool)atoi(argv[3]);
     bool iDoSort = (bool)atoi(argv[4]);
+    bool iDoFlush = (bool)atoi(argv[5]);
+    string iFlushDir = argv[6];
     
     // Initial MapFetcher
     MapFetcher mf(iFetchSize,  mapfilename.c_str());
@@ -59,7 +63,6 @@ int main(int argc, char **argv)
   
     // Add map entries to index
     struct timeval start, end;
-    double result;
     int cnt = 0;
     
     start = Util::Gettime();
@@ -68,7 +71,15 @@ int main(int argc, char **argv)
         //cout << hentry.show() << endl;
         if ( index_pool.count(hentry.id) == 0 ) {
             // index for this pid is not in pool
-            index_pool[hentry.id] = new Index(iDoMerge);
+            if ( iDoFlush ) {
+                ostringstream fpath;
+                fpath << iFlushDir << "/" 
+                      << "dropping.index." << hentry.id;
+                index_pool[hentry.id] = new Index( 
+                        fpath.str().c_str(), iDoMerge );
+            } else {
+                index_pool[hentry.id] = new Index(iDoMerge);
+            }
         }
         //cout << hentry.show() << endl;
         index_pool[hentry.id]->addWrite(
@@ -83,29 +94,38 @@ int main(int argc, char **argv)
 
 
     end = Util::Gettime();
-    result = Util::GetTimeDurAB(start, end);
+    double add_time = Util::GetTimeDurAB(start, end);
 
-    // clean up
+
+    // cleanup
     int newcnt = 0;
     map<pid_t, Index*>::iterator it;
+
+    start = Util::Gettime();
     for (it = index_pool.begin() ; it != index_pool.end() ; ++it) {
         newcnt += it->second->_hostIndex.size();
         if (iDoSort) {
             it->second->sortEntries(); // sort entries
         }
+        if (iDoFlush) {
+            it->second->flush();
+        }
         //cout << it->second->show();
         delete it->second;
     }
+    end = Util::Gettime();
+    double sort_flush_time = Util::GetTimeDurAB(start, end);
 
     // Print performance results
-    Performance perfs;
+    Performance perfs(20);
     perfs.put("prog_name", argv[0]);
     perfs.put("cnt", cnt);
-    perfs.put("time", result);
-    perfs.put("speed", (cnt/result));
+    perfs.put("add_time", add_time);
+    perfs.put("speed", (cnt/add_time));
     perfs.put("final_size", newcnt);
     perfs.put("do_merging", iDoMerge);
     perfs.put("do_sort", iDoMerge);
+    perfs.put("sort_flush_time", sort_flush_time);
 
     cout << perfs.showColumns();
 
