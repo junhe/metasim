e8e6e7df66ee6b051e797d5b7de6adbac94d3172
Jun He <junnhe@gmail.com>
2013-07-31 15:20:09 -0700

    added sortandmerge_bad

--- tools/wl_generator.py
+++ tools/wl_generator.py
@@ -1,7 +1,7 @@
 import sys
 
 if __name__ == "__main__":
-    choice = "sortandmerge_good"
+    choice = "sortandmerge_bad"
 
     if choice == "dumbmerge_good":
         wlen = 2
@@ -21,6 +21,22 @@ if __name__ == "__main__":
                         woff+wlen-1, wpid, wphyoff
     elif choice == "sortandmerge_good":
         wlen = 2
+        wstride = -2
+        local_nwrites = 100000
+        npid = 64
+        
+        for wpid in range(0, npid):
+            for i in range(0, local_nwrites):
+                woff = (wpid+1) * local_nwrites * abs(wstride) \
+                       + wstride * (i+1)
+                wphyoff = wlen * i
+                begin_t = i
+                end_t = i + 0.1
+
+                print wpid, "w", woff, wlen, begin_t, end_t, \
+                        woff+wlen-1, wpid, wphyoff
+    elif choice == "sortandmerge_bad":
+        wlen = 1 
         wstride = -2
         local_nwrites = 100000
         npid = 64
